701a96059f2573e13e4e2521d8c59eb424c6d243
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-09 03:58:40 +0100

    Add #all_rows, #table, #next_id, #insert

--- database.rb
+++ database.rb
@@ -13,12 +13,24 @@ class Database
     @connection.execute("CREATE TABLE IF NOT EXISTS #{table_name} (id INT PRIMARY KEY, length INT, created_at DATETIME, description VARCHAR(255))")
   end
 
-  def table?(table_name)
-    @connection.table_info(table_name).empty?? false : true
-  end
-
   def drop_table(table_name)
     @connection.execute("DROP TABLE IF EXISTS #{table_name}")
+  end
+
+  def insert(table_name)
+    @connection.execute("INSERT INTO #{table_name} (id) VALUES (?)", [@connection.last_insert_row_id + 1])
+  end
+
+  def next_id
+    @connection.last_insert_row_id
+  end
+
+  def all_rows(table_name)
+    @connection.execute("SELECT * FROM #{table_name}").first
+  end
+
+  def table?(table_name)
+    @connection.table_info(table_name).empty?? false : true
   end
 
   def eradicate
