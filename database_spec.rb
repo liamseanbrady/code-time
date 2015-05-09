5475fb91c0ef7ba2ffc593c0a963c1e7f4a81cbc
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-09 03:58:49 +0100

    Add more specs

--- database_spec.rb
+++ database_spec.rb
@@ -73,4 +73,39 @@ describe 'Database' do
       expect(File.exist?(db.file_path)).to eq(false)
     end
   end
+
+  describe '#all_rows' do
+    it 'returns the an array for the given name if it exists' do
+      db = Database.new
+      table_name = 'sessions'
+      db.add_table(table_name)
+      db.connection.execute('INSERT INTO sessions (id, length, created_at, description) VALUES (?, ?, ?, ?)', [db.next_id, 100, Time.now.to_s, 'hello'])
+     
+      expect(db.all_rows(table_name)).to be_a Hash
+      expect(db.all_rows(table_name).keys).to include('length')
+    end
+    it 'returns nil if no table exists for the given name'
+  end
+
+  describe '#insert' do
+    it 'inserts a new blank row into a table with the given name' do
+    # db = Database.new
+    # table_name = 'sessions'
+    # db.add_table(table_name)
+    # db.insert(table_name)
+
+    # expect(db.table(table_name).size).to eq(1)
+    end
+    it 'inserts a new full row into a table with the given name and keywords'
+  end
+
+  describe '#where_id' do
+  # it 'returns a hash from the database for the row with a given id' do
+  #   db = Database.new
+  #   table_name = 'sessions'
+  #   db.add_table(table_name)
+  #   db.
+  # end
+  # it 'returns an empty array if no row matches the given id'
+  end
 end
