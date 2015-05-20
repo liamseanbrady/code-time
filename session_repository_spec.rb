2a68b621f5768ad641322460696850b696ce1d08
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-21 00:35:40 +0100

    Use stub methods instead of using full double object

--- session_repository_spec.rb
+++ session_repository_spec.rb
@@ -42,40 +42,47 @@ end
 
 describe 'SessionRepository' do
   describe '#all' do
-    let(:adapter) { AdapterDouble.new }
-    let(:session_factory) { SessionFactoryDouble.new }
+    let(:adapter) { double('adapter') }
+    let(:session_factory) { double('session_factory') }
     let(:session_repo) { SessionRepository.new(adapter, session_factory) }
 
     it 'returns an empty array if there are no sessions stored' do
+      expect(adapter).to receive(:all) { [] }
+
       expect(session_repo.all).to eq([])
     end
 
     it 'returns an array containing one session if one session is stored' do
-      session_one = { id: 1, length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
+      session_one = double('session')
 
+      expect(session_factory).to receive(:make)
       expect(adapter).to receive(:all) { [session_one] }
       
-      expect(session_repo.all.last.length).to eq(5)
+      expect(session_repo.all.size).to eq(1)
     end
 
     it 'returns an array containing many sessions if many sessions are stored' do
-      session_one = { id: 1, length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
-      session_two = { id: 2, length: 10, description: 'Ruby is bad', created_at: Time.now.to_s }
+      session_one = double('session')
+      session_two = double('session')
 
+      expect(session_factory).to receive(:make).twice
       expect(adapter).to receive(:all) { [session_one, session_two] }
       
-      expect(session_repo.all.last.length).to eq(10)
+      expect(session_repo.all.size).to eq(2)
     end
   end
 
   describe '#store' do
-    let(:adapter) { AdapterDouble.new }
-    let(:session_factory) { SessionFactoryDouble.new }
+    let(:adapter) { double('adapter') }
+    let(:session_factory) { double('session_factory') }
     let(:session_repo) { SessionRepository.new(adapter, session_factory) }
 
     it 'stores a single session' do
       session_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
 
+      allow(session_factory).to receive(:make) { double('session') }
+      allow(adapter).to receive(:all) { [{ id: 1 }.merge(session_one)] }
+      expect(adapter).to receive(:save).with(session_one) 
       session_repo.store(session_one)
 
       expect(session_repo.all.count).to eq(1)
@@ -85,6 +92,10 @@ describe 'SessionRepository' do
       session_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
       session_two = { length: 1, description: 'TDD is good', created_at: Time.now.to_s }
 
+      allow(session_factory).to receive(:make).twice { double('session') }
+      allow(adapter).to receive(:all) { [{ id: 1 }.merge(session_one), { id: 2 }.merge(session_two)] }
+      expect(adapter).to receive(:save).with(session_one)
+      expect(adapter).to receive(:save).with(session_two)
       session_repo.store(session_one)
       session_repo.store(session_two)
 
@@ -93,8 +104,8 @@ describe 'SessionRepository' do
   end
 
   describe '#find_by_length' do
-    let(:adapter) { AdapterDouble.new }
-    let(:session_factory) { SessionFactoryDouble.new }
+    let(:adapter) { double('adapter') }
+    let(:session_factory) { double('session_factory') }
     let(:session_repo) { SessionRepository.new(adapter, session_factory) }
 
     it 'returns an empty array if no session matches' do
@@ -107,6 +118,10 @@ describe 'SessionRepository' do
         { id: 2, length: 20, description: 'TDD is good', created_at: Time.now.to_s },
       ]
 
+      session_one = double('session')
+      expect(session_one).to receive(:description) { all_sessions.first[:description] }
+      allow(session_factory).to receive(:make) { session_one }
+      expect(adapter).to receive(:find_by_length).with(10) { all_sessions.first }
       allow(adapter).to receive(:sessions).and_return(all_sessions)
      
       expect(session_repo.find_by_length(10).first.description).to eq('Ruby is good')
