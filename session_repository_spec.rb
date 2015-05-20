80633e7d4f8f601225f4ae4827a8736a6832a6e2
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-20 04:02:06 +0100

    Try out new AdapterDouble - it now works with hashes of real data instead of plain arrays of Object instances

--- session_repository_spec.rb
+++ session_repository_spec.rb
@@ -18,6 +18,26 @@ class AdapterDouble
   end
 end
 
+class AltAdapterDouble
+  def sessions
+    @sessions ||= []
+  end
+
+  def save(session_data)
+    @id ||= 1
+    sessions << { id: @id }.merge(session_data)
+    @id += 1
+  end
+
+  def all
+    sessions
+  end
+
+  def find_by_length(seconds)
+    sessions.reject { |session| session.fetch(:length) != seconds }
+  end
+end
+
 describe 'SessionRepository' do
   describe '#all' do
     it 'returns an empty array if there are no sessions stored' do
@@ -47,51 +67,61 @@ describe 'SessionRepository' do
 
   describe '#store' do
     it 'stores a single session' do
-      session_repo = SessionRepository.new(AdapterDouble.new)
-      session = double('session')
+      session_repo = SessionRepository.new(AltAdapterDouble.new)
+      session_data = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
 
-      session_repo.store(session)
+      session_repo.store(session_data)
 
-      expect(session_repo.all.first).to eq(session)
+      expect(session_repo.all.first).to eq({ id: 1 }.merge(session_data))
+    end
+
+    it 'stores a second session to a non-empty repository' do
+      session_repo = SessionRepository.new(AltAdapterDouble.new)
+      session_data_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
+      session_data_two = { length: 1, description: 'TDD is good', created_at: Time.now.to_s }
+
+      session_repo.store(session_data_one)
+      session_repo.store(session_data_two)
+
+      results_collection = [{ id: 1 }.merge(session_data_one), 
+                            { id: 2 }.merge(session_data_two)]
+
+      expect(session_repo.all).to match_array(results_collection)
     end
   end
 
   describe '#find_by_length' do
     it 'returns an empty array if no session matches' do
-      session_repo = SessionRepository.new(AdapterDouble.new)
+      session_repo = SessionRepository.new(AltAdapterDouble.new)
 
       expect(session_repo.find_by_length(10)).to eq([])
     end
 
     it 'returns a single session in an array if one session matches' do
-      session_repo = SessionRepository.new(AdapterDouble.new)
-      matching_session = double('session')
-      non_matching_session = double('session')
-      allow(matching_session).to receive(:length) { 10 }
-      allow(non_matching_session).to receive(:length) { 20 }
+      session_repo = SessionRepository.new(AltAdapterDouble.new)
+      matching_session = { length: 10, description: 'Ruby is good', created_at: Time.now.to_s }
+      non_matching_session = { length: 20, description: 'TDD is good', created_at: Time.now.to_s }
+
       session_repo.store(matching_session)
       session_repo.store(non_matching_session)
-
-      expect(session_repo.find_by_length(10)).to eq([matching_session])
+     
+      expect(session_repo.find_by_length(10)).to eq([{ id: 1 }.merge(matching_session)])
     end
 
     it 'returns many sessions in an array if many sessions match' do
-      session_repo = SessionRepository.new(AdapterDouble.new)
-      matching_session_one = double('session')
-      non_matching_session_one = double('session')
-      matching_session_two = double('session')
-      non_matching_session_two = double('session')
+      session_repo = SessionRepository.new(AltAdapterDouble.new)
+      matching_session_one = { length: 10, description: 'Ruby is good', created_at: Time.now.to_s }
+      non_matching_session_one = { length: 20, description: 'TDD is good', created_at: Time.now.to_s }
+      matching_session_two = { length: 10, description: 'Ruby is bad', created_at: Time.now.to_s }
+      non_matching_session_two = { length: 20, description: 'TDD is bad', created_at: Time.now.to_s }
 
-      allow(matching_session_one).to receive(:length) { 10 }
-      allow(non_matching_session_one).to receive(:length) { 20 }
-      allow(matching_session_two).to receive(:length) { 10 }
-      allow(non_matching_session_two).to receive(:length) { 40 }
       session_repo.store(matching_session_one)
       session_repo.store(non_matching_session_one)
       session_repo.store(matching_session_two)
       session_repo.store(non_matching_session_two)
-
-      expect(session_repo.find_by_length(10)).to match_array([matching_session_one, matching_session_two])
+     
+     # Actually, we want to return a Session object here made by the SessionFactory
+      expect(session_repo.find_by_length(10)).to eq([{ id: 1 }.merge(matching_session_one), { id: 3 }.merge(matching_session_two)])
     end
   end
 end
