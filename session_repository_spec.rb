0ee038ef968a521438632c0e3d5a172f62cbb121
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-20 20:34:30 +0100

    Add SessionDouble and SessionFactoryDouble. Stub #all and #find_by_length methods for the adapter

--- session_repository_spec.rb
+++ session_repository_spec.rb
@@ -14,7 +14,27 @@ class AdapterDouble
   end
 
   def find_by_length(seconds)
-    sessions.reject { |session| session.length != seconds }
+    sessions.reject { |session| session[:length] != seconds }
+  end
+end
+
+class SessionDouble
+  attr_reader :length, :description, :created_at
+
+  def initialize(length:, description:, created_at:)
+    @length = length
+    @description = description
+    @created_at = created_at
+  end
+
+  def attributes
+    { length: length, description: description, created_at: created_at }
+  end
+end
+
+class SessionFactoryDouble
+  def make(attributes)
+    SessionDouble.new(length: attributes[:length], description: attributes[:description], created_at: attributes[:created_at])
   end
 end
 
@@ -41,87 +61,90 @@ end
 describe 'SessionRepository' do
   describe '#all' do
     it 'returns an empty array if there are no sessions stored' do
-      session_repo = SessionRepository.new(AdapterDouble.new)
+      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
 
       expect(session_repo.all).to eq([])
     end
 
     it 'returns an array containing one session if one session is stored' do
-      session_repo = SessionRepository.new(AdapterDouble.new)
-      session = double('session')
-      session_repo.adapter.save(session)
+      adapter = AltAdapterDouble.new
+      session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
+
+      session_one = { id: 1, length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
+
+      expect(adapter).to receive(:all) { [session_one] }
       
-      expect(session_repo.all).to eq([session])
+      expect(session_repo.all.last.length).to eq(5)
     end
 
     it 'returns an array containing many sessions if many sessions are stored' do
-      session_repo = SessionRepository.new(AdapterDouble.new)
-      session_one = double('session')
-      session_two = double('session')
-      session_repo.adapter.save(session_one)
-      session_repo.adapter.save(session_two)
+      adapter = AltAdapterDouble.new
+      session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
+
+      session_one = { id: 1, length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
+      session_two = { id: 2, length: 10, description: 'Ruby is bad', created_at: Time.now.to_s }
+
+      expect(adapter).to receive(:all) { [session_one, session_two] }
       
-      expect(session_repo.all).to match_array([session_one, session_two])
+      expect(session_repo.all.last.length).to eq(10)
     end
   end
 
   describe '#store' do
     it 'stores a single session' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new)
-      session_data = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
+      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
+      session_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
 
-      session_repo.store(session_data)
+      session_repo.store(session_one)
 
-      expect(session_repo.all.first).to eq({ id: 1 }.merge(session_data))
+      expect(session_repo.all.count).to eq(1)
     end
 
     it 'stores a second session to a non-empty repository' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new)
-      session_data_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
-      session_data_two = { length: 1, description: 'TDD is good', created_at: Time.now.to_s }
+      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
+      session_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
+      session_two = { length: 1, description: 'TDD is good', created_at: Time.now.to_s }
 
-      session_repo.store(session_data_one)
-      session_repo.store(session_data_two)
+      session_repo.store(session_one)
+      session_repo.store(session_two)
 
-      results_collection = [{ id: 1 }.merge(session_data_one), 
-                            { id: 2 }.merge(session_data_two)]
-
-      expect(session_repo.all).to match_array(results_collection)
+      expect(session_repo.all.count).to eq(2)
     end
   end
 
   describe '#find_by_length' do
     it 'returns an empty array if no session matches' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new)
+      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
 
       expect(session_repo.find_by_length(10)).to eq([])
     end
 
     it 'returns a single session in an array if one session matches' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new)
-      matching_session = { length: 10, description: 'Ruby is good', created_at: Time.now.to_s }
-      non_matching_session = { length: 20, description: 'TDD is good', created_at: Time.now.to_s }
+      adapter = AltAdapterDouble.new
+      session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
+      all_sessions = [
+        { id: 1, length: 10, description: 'Ruby is good', created_at: Time.now.to_s },
+        { id: 2, length: 20, description: 'TDD is good', created_at: Time.now.to_s },
+      ]
 
-      session_repo.store(matching_session)
-      session_repo.store(non_matching_session)
+      allow(adapter).to receive(:sessions).and_return(all_sessions)
      
-      expect(session_repo.find_by_length(10)).to eq([{ id: 1 }.merge(matching_session)])
+      expect(session_repo.find_by_length(10).first.description).to eq('Ruby is good')
     end
 
     it 'returns many sessions in an array if many sessions match' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new)
-      matching_session_one = { length: 10, description: 'Ruby is good', created_at: Time.now.to_s }
-      non_matching_session_one = { length: 20, description: 'TDD is good', created_at: Time.now.to_s }
-      matching_session_two = { length: 10, description: 'Ruby is bad', created_at: Time.now.to_s }
-      non_matching_session_two = { length: 20, description: 'TDD is bad', created_at: Time.now.to_s }
+      adapter = AltAdapterDouble.new
+      session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
+      all_sessions = [
+        { id: 1, length: 10, description: 'Ruby is good', created_at: Time.now.to_s },
+        { id: 2, length: 20, description: 'TDD is good', created_at: Time.now.to_s },
+        { id: 3, length: 10, description: 'Ruby is bad', created_at: Time.now.to_s },
+        { id: 4, length: 20, description: 'TDD is bad', created_at: Time.now.to_s }
+      ]
 
-      session_repo.store(matching_session_one)
-      session_repo.store(non_matching_session_one)
-      session_repo.store(matching_session_two)
-      session_repo.store(non_matching_session_two)
+      allow(adapter).to receive(:sessions).and_return(all_sessions)
      
-     # Actually, we want to return a Session object here made by the SessionFactory
-      expect(session_repo.find_by_length(10)).to eq([{ id: 1 }.merge(matching_session_one), { id: 3 }.merge(matching_session_two)])
+      expect(session_repo.find_by_length(10).map(&:description)).to match_array(['Ruby is good', 'Ruby is bad'])
     end
   end
 end
