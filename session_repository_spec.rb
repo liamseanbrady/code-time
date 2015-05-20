43d0817f215b35ad5d77d7d46301eece274e73cb
Liam Sean Brady <liamseanbrady@gmail.com>
2015-05-20 20:39:32 +0100

    Change name of AltAdapterDouble class to AdapterDouble

--- session_repository_spec.rb
+++ session_repository_spec.rb
@@ -20,7 +20,7 @@ class SessionFactoryDouble
   end
 end
 
-class AltAdapterDouble
+class AdapterDouble
   def sessions
     @sessions ||= []
   end
@@ -43,13 +43,13 @@ end
 describe 'SessionRepository' do
   describe '#all' do
     it 'returns an empty array if there are no sessions stored' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
+      session_repo = SessionRepository.new(AdapterDouble.new, SessionFactoryDouble.new)
 
       expect(session_repo.all).to eq([])
     end
 
     it 'returns an array containing one session if one session is stored' do
-      adapter = AltAdapterDouble.new
+      adapter = AdapterDouble.new
       session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
 
       session_one = { id: 1, length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
@@ -60,7 +60,7 @@ describe 'SessionRepository' do
     end
 
     it 'returns an array containing many sessions if many sessions are stored' do
-      adapter = AltAdapterDouble.new
+      adapter = AdapterDouble.new
       session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
 
       session_one = { id: 1, length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
@@ -74,7 +74,7 @@ describe 'SessionRepository' do
 
   describe '#store' do
     it 'stores a single session' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
+      session_repo = SessionRepository.new(AdapterDouble.new, SessionFactoryDouble.new)
       session_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
 
       session_repo.store(session_one)
@@ -83,7 +83,7 @@ describe 'SessionRepository' do
     end
 
     it 'stores a second session to a non-empty repository' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
+      session_repo = SessionRepository.new(AdapterDouble.new, SessionFactoryDouble.new)
       session_one = { length: 5, description: 'Ruby is good', created_at: Time.now.to_s }
       session_two = { length: 1, description: 'TDD is good', created_at: Time.now.to_s }
 
@@ -96,13 +96,13 @@ describe 'SessionRepository' do
 
   describe '#find_by_length' do
     it 'returns an empty array if no session matches' do
-      session_repo = SessionRepository.new(AltAdapterDouble.new, SessionFactoryDouble.new)
+      session_repo = SessionRepository.new(AdapterDouble.new, SessionFactoryDouble.new)
 
       expect(session_repo.find_by_length(10)).to eq([])
     end
 
     it 'returns a single session in an array if one session matches' do
-      adapter = AltAdapterDouble.new
+      adapter = AdapterDouble.new
       session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
       all_sessions = [
         { id: 1, length: 10, description: 'Ruby is good', created_at: Time.now.to_s },
@@ -115,7 +115,7 @@ describe 'SessionRepository' do
     end
 
     it 'returns many sessions in an array if many sessions match' do
-      adapter = AltAdapterDouble.new
+      adapter = AdapterDouble.new
       session_repo = SessionRepository.new(adapter, SessionFactoryDouble.new)
       all_sessions = [
         { id: 1, length: 10, description: 'Ruby is good', created_at: Time.now.to_s },
